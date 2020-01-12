cd68f3bce6a37a250683b30539ba64fece6a90ce
Jordy Kingama <kingamattack@outlook.com>
2020-01-12 22:35:39 +0100

    Get Json and show

--- MVC/README.md
+++ MVC/README.md
@@ -1,2 +1,26 @@
 - Création d'un projet Multiplatform -> Appp -> Blank Forms App -> App name -> Android/iOS / -> Use.NET Standard (not Shared Library)
 - Mettre à jours les paquets
+- Design de la vue 
+- Binding context -> this
+- Data Bidning 
+- Android clear htttp -> true (Research)
+https://medium.com/@son.rommer/fix-cleartext-traffic-error-in-android-9-pie-2f4e9e2235e6
+- Classe Show and JsonShow 
+```
+public class Show
+{
+    public int Id { get; set; }
+    public string Url { get; set; }
+    public string Name { get; set; }
+    public string Language { get; set; }
+    public List<string> Genres { get; set; }
+}
+
+public class JsonShow
+{
+    public double Score { get; set; }
+    public Show Show { get; set; }
+}
+```
+
+
--- MVC/tvshows/tvshows/MainPage.xaml.cs
+++ MVC/tvshows/tvshows/MainPage.xaml.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
+using System.Net.Http;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
 
 namespace tvshows
@@ -13,9 +16,58 @@ namespace tvshows
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        public ObservableCollection<string> Values { get; set; } = new ObservableCollection<string>();
+        
+
+        private readonly List<string> movies = new List<string>
+        {
+            "Avengers",
+            "Forest Gump",
+            "Spiderman",
+            "Superman",
+            "Black Panther",
+            "Venom",
+            "Iron man",
+            "Captain America",
+            "Dr. Strange",
+            "Thor",
+            "Hulk",
+            "She Hulk",
+            "Gardiens"
+        };
+
         public MainPage()
         {
             InitializeComponent();
+
+            BindingContext = this;
+        }
+
+        async void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var searchBar = (SearchBar)sender;
+
+            if(searchBar.Text.Length >= 3)
+            {
+                await GetData(searchBar.Text);
+            }
+        }
+
+        private async Task GetData(string query)
+        {
+            var httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync($"http://api.tvmaze.com/search/shows?q={query}");
+
+            if(response.IsSuccessStatusCode)
+            {
+                string data = await response.Content.ReadAsStringAsync();
+                Debug.WriteLine($"Value: {data}");
+
+                var list = JsonConvert.DeserializeObject<List<JsonShow>>(data);
+
+                var show = list[0];
+            }            
         }
     }
 }
