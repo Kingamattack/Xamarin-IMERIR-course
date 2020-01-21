7417e7ad93a89a79fc67844fb46330886a2bc9ac
Jordy Kingama <kingamattack@outlook.com>
2020-01-21 01:27:41 +0100

    Add DetailsPage

---
+++ MVC/tvshows/tvshows/Views/DetailsPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿// File: DetailsPage.xaml.cs
+// Author: jordy
+// Date: 21/1/2020
+
+using Xamarin.Forms;
+
+namespace tvshows.Views
+{
+    public partial class DetailsPage : ContentPage
+    {
+        public DetailsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
